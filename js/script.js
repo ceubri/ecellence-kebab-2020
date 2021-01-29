4ec90610af470b5ba7517c1395d47a05d640bb09
ceubri <ceubrak@yahoo.fr>
2021-01-29 18:56:36 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1400},"pages":[{"id":"accueil","displayName":"Accueil","link":{"linkType":"LinkTypePage","href":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"menus","displayName":"Menus","link":{"linkType":"LinkTypePage","href":"#!page-menus"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1400},"pages":[{"id":"accueil","displayName":"Accueil","link":{"type":"LinkTypePage","value":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"menus","displayName":"Menus","link":{"type":"LinkTypePage","value":"#!page-menus"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"contact","displayName":"Contact","link":{"type":"LinkTypePage","value":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
 
         /*
          * active menu widget for Silex
